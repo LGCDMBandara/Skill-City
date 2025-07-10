4bc40ec51766f8bf7619049d2741119fdaf5f7f5
Ahaz Fernando <ahazfernando@gmail.com>
2025-07-10 06:14:43 +0530

    Contact Forms UI changes

--- frontend/src/Contact/Contact.jsx
+++ frontend/src/Contact/Contact.jsx
@@ -6,8 +6,9 @@ import { FaXTwitter } from "react-icons/fa6";
 import emailjs from '@emailjs/browser';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { LoaderIcon, MailIcon, PhoneCallIcon } from 'lucide-react';
 
-const contact = () => {
+const Contact = () => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -57,45 +58,47 @@ const contact = () => {
     };
 
     return (
-        <div>
+        <div className="contact-main-container">
             <ToastContainer />
-            <div>
-                <div className="contacts-box-container bg-[#53d74e] min-h-screen p-4">
-                    <div className="contacts-box-left">
-                        <p className="contacts-box-title">Get in Touch</p>
-                        <p className="contacts-box-description">Have a question or need a quote? Fill out the form below and our team will get back to you promptly to discuss your cleaning or labor hire needs.</p>
-                        <div className="contacts-details">
-                            <p className="contacts-detail"><FaMapMarkerAlt /> Address : 4A Haughton Road, Oakleigh, 3166, VIC</p>
-                            <p className="contacts-detail"><FaPhoneAlt /> Call Us : 039 034 6492</p>
-                            <p className="contacts-detail"><FaEnvelope /> Email Us : admin@skillcityfs.com.au</p>
+            <div className="contact-container">
+                <h1 className="contact-title">Contact Us Today</h1>
+                <p className="contact-subtitle">Expert Cleaning & Building Services at Your Fingertips - Contact Us Today!</p>
+                <div className="contact-box-container">
+                    <div className="contact-box-left">
+                        <p className="contact-box-title">Get in Touch</p>
+                        <p className="contact-box-description">Have a question or need a quote? Fill out the form below and our team will get back to you promptly to discuss your cleaning or labor hire needs.</p>
+                        <div className="contact-details">
+                            <p className="contact-detail"><LoaderIcon /> Address : 4A Haughton Road, Oakleigh, 3166, VIC</p>
+                            <div className="contact-detail-row">
+                                <p className="contact-detail"><PhoneCallIcon /> Call Us : 039 034 6492</p>
+                                <p className="contact-detail"><MailIcon /> Email Us : admin@skillcityfs.com.au</p>
+                            </div>
                         </div>
-                        <div className="contacts-bottom">
+                        <div className="contact-bottom">
                             <div>
-                                <p className="contacts-detail">Follow Us</p>
-                                <div className="contacts-social-icons">
+                                <p className="contact-follow" style={{ color: 'white' }}>Follow Us</p>
+                                <div className="contact-social-icons">
                                     <a href="https://linkedin.com/" target="_blank" rel="noopener noreferrer">
-                                        <FaLinkedin className='contacts-icons' />
+                                        <FaLinkedin className='contact-icon' style={{ color: 'white' }} />
                                     </a>
                                     <a href="https://facebook.com/" target="_blank" rel="noopener noreferrer">
-                                        <FaFacebook className='contacts-icons' />
+                                        <FaFacebook className='contact-icon' style={{ color: 'white' }} />
                                     </a>
                                     <a href="https://instagram.com/" target="_blank" rel="noopener noreferrer">
-                                        <FaInstagram className='contacts-icons' />
+                                        <FaInstagram className='contact-icon' style={{ color: 'white' }} />
                                     </a>
                                     <a href="https://x.com/?lang=en" target="_blank" rel="noopener noreferrer">
-                                        <FaXTwitter className='contacts-icons' />
+                                        <FaXTwitter className='contact-icon' style={{ color: 'white' }} />
                                     </a>
                                     <a href="https://tiktok.com/" target="_blank" rel="noopener noreferrer">
-                                        <FaTiktok className='contacts-icons' />
+                                        <FaTiktok className='contact-icon' style={{ color: 'white' }} />
                                     </a>
                                 </div>
                             </div>
-                            <div className="contacts-box-image-placeholder">
-                                <img src={assets.ContactImage} alt="Contact Image" />
-                            </div>
                         </div>
                     </div>
-                    <div className="contacts-box-right">
+                    <div className="contact-box-right">
+                        <p className="contact-box-subtitle">Send Us a Message</p>
                         <form onSubmit={handleSubmit} className="contact-form">
                             <h4>Name</h4>
                             <input
@@ -106,23 +109,28 @@ const contact = () => {
                                 onChange={handleChange}
                                 required />
 
-                            <h4>Mobile Number</h4>
-                            <input
-                                type="text"
-                                placeholder="Mobile Number"
-                                value={formData.phone}
-                                name="phone"
-                                onChange={handleChange}
-                                required />
-
-                            <h4>Email</h4>
-                            <input
-                                type="email"
-                                placeholder="Email Address"
-                                name="email"
-                                value={formData.email}
-                                onChange={handleChange}
-                                required />
+                            <div className="contact-form-row">
+                                <div className="contact-form-column">
+                                    <h4>Mobile Number</h4>
+                                    <input
+                                        type="text"
+                                        placeholder="Mobile Number"
+                                        value={formData.phone}
+                                        name="phone"
+                                        onChange={handleChange}
+                                        required />
+                                </div>
+                                <div className="contact-form-column">
+                                    <h4>Email</h4>
+                                    <input
+                                        type="email"
+                                        placeholder="Email Address"
+                                        name="email"
+                                        value={formData.email}
+                                        onChange={handleChange}
+                                        required />
+                                </div>
+                            </div>
 
                             <h4>Subject</h4>
                             <input
@@ -141,7 +149,7 @@ const contact = () => {
                                 onChange={handleChange}
                                 required />
 
-                            <button className="send-btn">Send Message</button>
+                            <button className="contact-send-btn">Send Message</button>
                         </form>
                     </div>
                 </div>
@@ -150,4 +158,4 @@ const contact = () => {
     );
 }
 
-export default contact;
+export default Contact;
